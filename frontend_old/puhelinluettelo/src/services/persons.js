012c03dca739711eb591c4eeb0fe2b3e1b3b05b8
arttu <ttu91@hotmail.com>
2021-08-15 15:19:53 +0300

    Build added

---
+++ frontend_old/puhelinluettelo/src/services/persons.js
@@ -0,0 +1,35 @@
+
+import axios from 'axios'
+
+const url = "http://localhost:3001/api/persons"
+
+const getAll = (callBack) => {
+  let result = axios
+                  .get(url)
+                  .then(response => {
+                    callBack(response.data)
+                  })
+  return result
+}
+
+const create = person => {
+    const request = axios.post(url, person)
+    return (request.then(response => response.data))
+}
+
+const deletePerson = (person_id, callBack, errorCallBack) => {
+  const request = axios.delete(`${url}/${person_id}`)
+  .then(response => {callBack()}).catch((error) => errorCallBack())
+  return(request)
+}
+
+const update = person => {
+
+  const request = axios.put(`${url}/${person.id}`, person)
+  return (request)
+}
+
+
+export default {
+    getAll, create, deletePerson, update
+}
